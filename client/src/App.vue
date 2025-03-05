e223697d7dd470fbbaa0e531e23adee82d6bae64
Roman Galanov <mongo4558@gmail.com>
2025-03-05 14:10:16 +0400

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import axios from "axios";
+import {reactive} from "vue";
+
+const config = reactive({
+  deviceName: 'test',
+  temperature: '10',
+  humidity: '89',
+})
+
+const saveConfig = async () => {
+  try {
+    const response = await axios.post("/api/configs", config);
+    console.log('Saved: ', response.data);
+  } catch (error) {
+    console.error('Error: ', error.response.data);
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <nav class="navigation">
+      <a href="">КАИ Iot</a>
+      <a href="">Прагма</a>
+      <a href="">Сигма</a>
+      <button type="submit" @click="saveConfig">Save</button>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ server/src/index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import {v4 as uuidv4} from 'uuid';
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.post('/configs', async (req, res) => {
+    try {
+        const configData = req.body;
+
+        if (!configData) {
+            return res.status(400).json({error: 'No configuration found.'});
+        }
+
+        const configDir = path.join(__dirname, 'configs');
+        await fs.mkdir(configDir, { recursive: true });
+
+        const fileName = `config-${uuidv4()}.json`;
+        const filePath = path.join(configDir, fileName);
+        await fs.writeFile(filePath, JSON.stringify(configData, null, 2));
+
+        res.json({
+            status: 'success',
+            path: filePath,
+        })
+    } catch (e) {
+        console.error('Server error: ', e);
+        res.status(500).json({error: e.message});
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
